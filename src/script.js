b0f0e659d0f6144decef4e8abbb7e2d4dd21804d
Alexis989898 <alexisvitor6@gmail.com>
2024-06-17 16:18:49 -0300

    Most functionalities working

--- src/script.js
+++ src/script.js
@@ -1,12 +1,19 @@
+// Add Task vars
 const addTaskBtn = document.getElementById("btn-add-task");
 const modalAddTask = document.getElementById("modal-add-task");
 const submitTaskBtn = document.getElementById("btn-submit-task");
 const addTaskForm = document.getElementById("add-task-form");
 
+// Add List vars
 const addListBtn = document.getElementById("btn-add-list");
 const modalAddList = document.getElementById("modal-add-list");
 const submitListBtn = document.getElementById("btn-submit-list");
 const addListForm = document.getElementById("add-list-form");
+
+// Edit task vars
+const editTaskBtn = document.getElementById("btn-edit-task");
+const modalEditTask = document.getElementById("modal-edit-task");
+const editTaskForm = document.getElementById("edit-task-form");
 
 const body = document.body;
 
@@ -25,7 +32,8 @@ function loadAllTasks() {
     // Load tasks from 'All Tasks'
     const allTasks = JSON.parse(localStorage.getItem('All Tasks')) || [];
     allTasks.forEach((task) => {
-        createTaskDiv(taskListDiv, task);
+        const taskDiv = createTaskElement(task);
+        taskListDiv.appendChild(taskDiv);
     });
 
     // Load tasks from all other lists
@@ -34,16 +42,17 @@ function loadAllTasks() {
         const tasks = JSON.parse(localStorage.getItem(item));
         if (tasks != null) {
             tasks.forEach((task) => {
-                createTaskDiv(taskListDiv, task);
+                const taskDiv = createTaskElement(task);
+                taskListDiv.appendChild(taskDiv);
             });
         }
     });
 }
 
 // Creates a task div
-function createTaskDiv(taskListDiv, task) {
+function createTaskElement(task) {
     const taskDiv = document.createElement("div");
-    taskDiv.className = "task";
+    taskDiv.className = `task task-priority-${task.priority}`;
 
     const taskNameDiv = document.createElement("div");
     taskNameDiv.className = "task-name";
@@ -59,14 +68,21 @@ function createTaskDiv(taskListDiv, task) {
     infoImg.className = "icon-btn";
     infoImg.src = "../imgs/info-icon.png";
     infoImg.alt = "info button";
+    infoImg.addEventListener("click", function (e) {
+        editTask(e, task);
+    });
     taskBtnInfoDiv.appendChild(infoImg);
 
     const taskBtnDeleteDiv = document.createElement("div");
     taskBtnDeleteDiv.className = "task-btn-delete";
+
     const deleteImg = document.createElement("img");
     deleteImg.className = "icon-btn";
     deleteImg.src = "../imgs/delete-icon.png";
     deleteImg.alt = "delete button";
+    deleteImg.addEventListener("click", function () {
+        deleteTask(task.name, task.listName);
+    });
     taskBtnDeleteDiv.appendChild(deleteImg);
 
     taskDiv.appendChild(taskNameDiv);
@@ -74,28 +90,43 @@ function createTaskDiv(taskListDiv, task) {
     taskDiv.appendChild(taskBtnInfoDiv);
     taskDiv.appendChild(taskBtnDeleteDiv);
 
-    taskListDiv.appendChild(taskDiv);
+    return taskDiv;
 }
 
 // Loads lists in the sidebar
 function loadLists() {
     const sidebarWrapper = document.querySelector(".sidebar-lists-wrapper");
     sidebarWrapper.innerHTML = "";
-    
+
     const lists = JSON.parse(localStorage.getItem('lists')) || [];
-    
+
     const allTasks = document.createElement("div");
     allTasks.className = "";
     allTasks.textContent = "All Tasks";
-    allTasks.onclick = function() { loadAllTasks() };
+    allTasks.onclick = function () { loadAllTasks() };
     allTasks.className = "sidebar-item sidebar-all-tasks";
     sidebarWrapper.appendChild(allTasks);
-
     lists.forEach((item) => {
         const list = document.createElement("div");
-        list.className = 'sidebar-item sidebar-list';
+        list.className = "sidebar-item sidebar-list";
         list.textContent = item;
-        list.onclick = function() { loadList(item) };
+        list.onclick = function () { loadList(item) };
+
+        const deleteBtn = document.createElement("div");
+        deleteBtn.className = "list-btn-delete";
+        deleteBtn.addEventListener("click", function (e) {
+            e.stopPropagation();
+            const listName = e.target.closest(".sidebar-item").textContent.trim();
+            deleteList(listName);
+        });
+
+        const deleteImg = document.createElement("img");
+        deleteImg.className = "list-icon-btn";
+        deleteImg.src = "../imgs/delete-icon.png";
+        deleteImg.alt = "delete button";
+
+        deleteBtn.appendChild(deleteImg);
+        list.appendChild(deleteBtn);
         sidebarWrapper.appendChild(list);
     });
 
@@ -111,41 +142,10 @@ function loadList(listName) {
 
     const listTasks = JSON.parse(localStorage.getItem(listName)) || [];
     listTasks.forEach((task) => {
-        const taskDiv = document.createElement("div");
-        taskDiv.className = "task";
-
-        const taskNameDiv = document.createElement("div");
-        taskNameDiv.className = "task-name";
-        taskNameDiv.textContent = task.name;
-
-        const taskDateDiv = document.createElement("div");
-        taskDateDiv.className = "task-date";
-        taskDateDiv.textContent = task.date;
-
-        const taskBtnInfoDiv = document.createElement("div");
-        taskBtnInfoDiv.className = "task-btn-info";
-        const infoImg = document.createElement("img");
-        infoImg.className = "icon-btn";
-        infoImg.src = "../imgs/info-icon.png";
-        infoImg.alt = "info button";
-        taskBtnInfoDiv.appendChild(infoImg);
-
-        const taskBtnDeleteDiv = document.createElement("div");
-        taskBtnDeleteDiv.className = "task-btn-delete";
-        const deleteImg = document.createElement("img");
-        deleteImg.className = "icon-btn";
-        deleteImg.src = "../imgs/delete-icon.png";
-        deleteImg.alt = "delete button";
-        taskBtnDeleteDiv.appendChild(deleteImg);
-
-        taskDiv.appendChild(taskNameDiv);
-        taskDiv.appendChild(taskDateDiv);
-        taskDiv.appendChild(taskBtnInfoDiv);
-        taskDiv.appendChild(taskBtnDeleteDiv);
-
+        const taskDiv = createTaskElement(task);
         taskListDiv.appendChild(taskDiv);
     });
-};
+}
 
 // Adds a task to the current list
 function submitTask() {
@@ -153,22 +153,33 @@ function submitTask() {
     const taskDescription = document.querySelector(".form-task-description").value.trim();
     const taskDate = document.querySelector(".form-task-date").value;
     const taskPriority = document.querySelector("input[name='priority']:checked").value;
+    const list = document.querySelector(".main-list-title").textContent;
+    const tasks = JSON.parse(localStorage.getItem(list)) || [];
+
+
+    if (hasDuplicateName(tasks, taskName)) {
+        alert("Error: Task name already exists!");
+        return; // Prevent further execution if duplicate is found
+    }
 
     const task = {
         name: taskName,
         description: taskDescription,
         date: taskDate,
-        priority: taskPriority
+        priority: taskPriority,
+        listName: list
     };
 
-    const list = document.querySelector(".main-list-title").textContent;
-    let tasks = JSON.parse(localStorage.getItem(list)) || [];
     tasks.push(task);
 
     localStorage.setItem(list, JSON.stringify(tasks));
 
     modalAddTask.style.display = "none";
-    loadList(list);
+    if (list == "All Tasks") {
+        loadAllTasks();
+    } else {
+        loadList(list);
+    }
 }
 
 // Adds a list
@@ -192,13 +203,6 @@ addTaskBtn.addEventListener("click", (e) => {
     e.stopPropagation();
 });
 
-// Closes Add Task modal
-body.addEventListener("click", (e) => {
-    if (modalAddTask.open && !modalAddTask.contains(e.target)) {
-        modalAddTask.style.display = "none";
-    }
-});
-
 // Opens Add List Modal
 addListBtn.addEventListener("click", (e) => {
     modalAddList.show();
@@ -206,9 +210,113 @@ addListBtn.addEventListener("click", (e) => {
     e.stopPropagation();
 });
 
-// Closes Add List Modal
+// Closes Modals
 body.addEventListener("click", (e) => {
     if (modalAddList.open && !modalAddList.contains(e.target)) {
         modalAddList.style.display = "none";
     }
+    if (modalAddTask.open && !modalAddTask.contains(e.target)) {
+        modalAddTask.style.display = "none";
+    }
+    if (modalEditTask.open && !modalEditTask.contains(e.target)) {
+        modalEditTask.style.display = "none";
+    }
 });
+
+// Delete task
+function deleteTask(taskName, listName) {
+    const mainListTitle = document.querySelector(".main-list-title").textContent;
+    let tasks = JSON.parse(localStorage.getItem(listName)) || [];
+    tasks = tasks.filter(task => task.name !== taskName);
+    localStorage.setItem(listName, JSON.stringify(tasks));
+    if (mainListTitle == "All Tasks") {
+        loadAllTasks();
+    } else {
+        loadList(mainListTitle);
+    }
+}
+
+// Delete list
+function deleteList(listName) {
+    let lists = JSON.parse(localStorage.getItem('lists')) || [];
+    lists = lists.filter(list => list !== listName);
+    localStorage.setItem('lists', JSON.stringify(lists));
+    localStorage.removeItem(listName); // Remove the list's tasks
+    loadLists();
+    loadAllTasks();
+}
+
+// Opens Edit Task Modal
+function editTask(e, task) {
+    modalEditTask.show();
+    modalEditTask.style.display = "flex";
+    e.stopPropagation();
+
+    const taskName = document.querySelector(".form-edit-task-name");
+    const taskDesc = document.querySelector(".form-edit-task-description");
+    const taskDate = document.querySelector(".form-edit-task-date");
+
+    const radioLow = document.querySelector(".radio-edit-low");
+    const radioMedium = document.querySelector(".radio-edit-medium");
+    const radioHigh = document.querySelector(".radio-edit-high");
+
+    const originalTaskNameInput = document.querySelector("input[name='original-task-name']");
+    originalTaskNameInput.value = task.name;
+
+    const taskList = document.querySelector("input[name='task-list']");
+    taskList.value = task.listName;
+
+    taskName.value = task.name;
+    taskDesc.value = task.description;
+    taskDate.value = task.date;
+    if (task.priority == "low") {
+        radioLow.checked = true;
+    } else if (task.priority == "medium") {
+        radioMedium.checked = true;
+    } else {
+        radioHigh.checked = true;
+    }
+}
+
+function submitEditedTask() {
+    const taskName = document.querySelector(".form-edit-task-name").value.trim();
+    const taskDescription = document.querySelector(".form-edit-task-description").value.trim();
+    const taskDate = document.querySelector(".form-edit-task-date").value;
+    const taskPriority = document.querySelector("input[name='priority-edit']:checked").value;
+    const list = document.querySelector("input[name='task-list']").value;
+
+    const editedTask = {
+        name: taskName,
+        description: taskDescription,
+        date: taskDate,
+        priority: taskPriority,
+        listName: list
+    };
+
+    let tasks = JSON.parse(localStorage.getItem(list)) || [];
+    const originalTaskName = document.querySelector("input[name='original-task-name']").value;
+    
+    // Check for duplicates excluding the original name
+    if (hasDuplicateName(tasks.filter(task => task.name !== originalTaskName), taskName)) {
+      alert("Error: Task name already exists!");
+      return; // Prevent update if duplicate is found
+    }
+
+    const taskIndex = tasks.findIndex(task => task.name === originalTaskName);
+
+    if (taskIndex > -1) {
+        tasks[taskIndex] = editedTask;
+        localStorage.setItem(list, JSON.stringify(tasks));
+    }
+
+    modalEditTask.style.display = "none";
+    if (list === "All Tasks") {
+        loadAllTasks();
+    } else {
+        loadList(list);
+    }
+}
+
+function hasDuplicateName(tasks, name) {
+    return tasks.some(task => task.name.toLowerCase() === name.toLowerCase()); // Case-insensitive check
+}
